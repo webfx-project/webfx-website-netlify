848be23ac4869f1f01fefcd42cae64c5d03cb8f1
salmonb <dev.salmonb@gmail.com>
2022-07-22 13:16:24 +0000

    Update from https://github.com/webfx-project/webfx-website/commit/4c655da93335442ad827f6ec4fe7cd8bd072b604

---
+++ dev/webfx/website/application/images/ImageLoader.java
@@ -0,0 +1,22 @@
+package dev.webfx.website.application.images;
+
+import dev.webfx.kit.launcher.WebFxKitLauncher;
+import dev.webfx.platform.resource.Resource;
+import javafx.scene.image.ImageView;
+
+/**
+ * @author Bruno Salmon
+ */
+public final class ImageLoader {
+
+    public static ImageView loadImage(String resourcePath) {
+        return new ImageView(toImageUrl(resourcePath));
+    }
+
+    public static String toImageUrl(String resourcePath) {
+        if (WebFxKitLauncher.supportsWebP())
+            resourcePath = resourcePath.replace(".png", ".webp");
+        return Resource.toUrl(resourcePath, ImageLoader.class);
+    }
+
+}
