ad1f0d03bb78869f6c80bbb8e254ad25802cd2bd
salmonb <maintainer@webfx.dev>
2022-08-23 13:29:26 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/c09d9efef2623732f24555096d384949929ff20e

--- dev/webfx/demo/demofx/DemoFXApplication.java
+++ dev/webfx/demo/demofx/DemoFXApplication.java
@@ -3,6 +3,8 @@ package dev.webfx.demo.demofx;
 import com.chrisnewland.demofx.DemoConfig;
 import com.chrisnewland.demofx.DemoFX;
 import com.chrisnewland.demofx.effect.IEffect;
+import com.chrisnewland.demofx.effect.addon.AddOnFadeInEffect;
+import com.chrisnewland.demofx.effect.addon.AddOnFadeOutEffect;
 import com.chrisnewland.demofx.effect.effectfactory.IEffectFactory;
 import com.chrisnewland.demofx.effect.fake3d.StarfieldSprite;
 import com.chrisnewland.demofx.effect.fractal.FractalRings;
@@ -11,19 +13,13 @@ import com.chrisnewland.demofx.effect.text.WordSearch;
 import dev.webfx.platform.resource.Resource;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class DemoFXApplication extends Application {
 
     private final StackPane root = new StackPane();
     private final Scene scene = new Scene(root, 800, 600);
-
-    private DemoFX demoFX;
     private boolean started;
 
     @Override
@@ -31,40 +27,38 @@ public class DemoFXApplication extends Application {
         stage.setTitle("DemoFX");
         stage.setScene(scene);
         stage.show();
-        DemoConfig config = new DemoConfig(scene.getWidth(), scene.getHeight());
-        demoFX = new DemoFX(config, (IEffectFactory) conf -> dev.webfx.platform.util.collection.Collections.listOf(
-                new WordSearch(conf, "Animation using DemoFX\n\nA JavaFX Canvas library\n\nby Chris Newland"),
-                new TextWaveSprite(conf, new String[] {"Click to start"}, conf.getHeight() - 200, 1, 5, true)
+        DemoFX playDemo = new DemoFX(newDemoConfig(null), (IEffectFactory) demoConfig -> dev.webfx.platform.util.collection.Collections.listOf(
+                new WordSearch(demoConfig, "Animation using DemoFX\n\nA JavaFX Canvas library\n\nby Chris Newland"),
+                new TextWaveSprite(demoConfig, new String[] {"Click to play"}, demoConfig.getHeight() - 200, 1, 5, true)
         ));
 
-        root.getChildren().setAll(demoFX.getPane());
-        demoFX.runDemo();
+        root.getChildren().setAll(playDemo.getPane());
+        playDemo.runDemo();
         root.setOnMouseClicked(e -> {
             if (!started) {
-                demoFX.stopDemo();
-                config.setAudioFilename(Resource.toUrl("DemoFX3.mp3", DemoFXApplication.class));
-                demoFX = new DemoFX(config, (IEffectFactory) conf -> dev.webfx.platform.util.collection.Collections.listOf(
-                        new StarfieldSprite(conf),
-                        schedule(new FractalRings(conf), 16_000, -1)
+                playDemo.stopDemo();
+                DemoFX demo = new DemoFX(newDemoConfig("DemoFX3.mp3"), (IEffectFactory) demoConfig -> dev.webfx.platform.util.collection.Collections.listOf(
+                        scheduleEffect(new AddOnFadeOutEffect(new StarfieldSprite(demoConfig)), 0, 26000),
+                        scheduleEffect(new AddOnFadeInEffect(new FractalRings(demoConfig)), 16000, -1)
                 ));
-                root.getChildren().setAll(demoFX.getPane());
-                demoFX.runDemo();
+                root.getChildren().setAll(demo.getPane());
+                demo.runDemo();
                 started = true;
             }
         });
     }
 
-    private IEffect schedule(IEffect effect, long start, long stop) {
+    private DemoConfig newDemoConfig(String audioResource) {
+        DemoConfig demoConfig = new DemoConfig(scene.getWidth(), scene.getHeight());
+        demoConfig.setAudioFilename(Resource.toUrl(audioResource, DemoFXApplication.class));
+        return demoConfig;
+    }
+
+    private IEffect scheduleEffect(IEffect effect, long start, long stop) {
         if (start > 0)
             effect.setStartOffsetMillis(start);
         if (stop > 0)
             effect.setStopOffsetMillis(stop);
         return effect;
     }
-
-    private static <R extends Region> R setBackgroundColor(Color color, R region) {
-        region.setBackground(new Background(new BackgroundFill(color, null, null)));
-        return region;
-    }
-
 }
