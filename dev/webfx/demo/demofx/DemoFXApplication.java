84c55658b1c6b5fa84b5401bad6ea3eb7d237483
salmonb <maintainer@webfx.dev>
2022-08-24 08:58:50 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/511b6913acd5809796cebdecf26ae09d7181b071

--- dev/webfx/demo/demofx/DemoFXApplication.java
+++ dev/webfx/demo/demofx/DemoFXApplication.java
@@ -3,21 +3,19 @@ package dev.webfx.demo.demofx;
 import com.chrisnewland.demofx.DemoConfig;
 import com.chrisnewland.demofx.DemoFX;
 import com.chrisnewland.demofx.effect.IEffect;
+import com.chrisnewland.demofx.effect.addon.RotateAddOnEffect;
 import com.chrisnewland.demofx.effect.effectfactory.IEffectFactory;
 import com.chrisnewland.demofx.effect.fake3d.SnowfieldSprite;
 import com.chrisnewland.demofx.effect.fake3d.StarfieldSprite;
 import com.chrisnewland.demofx.effect.fractal.FractalRings;
 import com.chrisnewland.demofx.effect.fractal.Mandelbrot;
 import com.chrisnewland.demofx.effect.fractal.Sierpinski;
-import com.chrisnewland.demofx.effect.shape.Checkerboard;
-import com.chrisnewland.demofx.effect.shape.Chord;
-import com.chrisnewland.demofx.effect.shape.Glowboard;
-import com.chrisnewland.demofx.effect.shape.SineLines;
+import com.chrisnewland.demofx.effect.shape.*;
 import com.chrisnewland.demofx.effect.sprite.Spin;
 import com.chrisnewland.demofx.effect.sprite.Tiles;
+import com.chrisnewland.demofx.effect.text.TextRing;
 import com.chrisnewland.demofx.effect.text.TextWaveSprite;
 import com.chrisnewland.demofx.effect.text.WordSearch;
-import com.chrisnewland.demofx.util.ShapeEffect;
 import dev.webfx.platform.resource.Resource;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -31,7 +29,7 @@ public class DemoFXApplication extends Application {
     private final Scene scene = new Scene(root, 1600, 1200);
     private DemoFX demoFX;
     private boolean started;
-    long t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11;
+    long t1, t2, t3, t4, t5, t6, t7, t8, t9;
 
     @Override
     public void start(Stage stage) {
@@ -50,19 +48,20 @@ public class DemoFXApplication extends Application {
                 demoFX.stopDemo();
                 demoFX = new DemoFX(newDemoConfig("DemoFX3.mp3"), (IEffectFactory) demoConfig -> dev.webfx.platform.util.collection.Collections.listOf(
                         scheduleEffect(new StarfieldSprite(demoConfig), 0, t1 = 16200),
-                        scheduleEffect(new FractalRings(demoConfig), t1, t3 = 40000),
-                        scheduleEffect(new Sierpinski(demoConfig), t2 = 32000, t3),
-                        scheduleEffect(new Mandelbrot(demoConfig), t3, t5 = 64000),
-                        scheduleEffect(new TextWaveSprite(demoConfig, new String[] {"Realtime computation"}, demoConfig.getHeight() - 200, 0.8, 10), t3 + 2000, t5 = 64000),
-                        scheduleEffect(new Checkerboard(demoConfig), t5, t6 = 70000),
-                        scheduleEffect(new Chord(demoConfig, Color.ORANGE), t4 = 48300, t6),
-                        scheduleEffect(new Glowboard(demoConfig), t6, t7 = 96000),
-                        scheduleEffect(new SineLines(demoConfig), t6, t7),
-                        scheduleEffect(new Tiles(demoConfig), t7, t8 = 160200),
-                        scheduleEffect(new ShapeEffect(demoConfig, 5, true), 113000, t8),
-                        scheduleEffect(new Spin(demoConfig), t8, t9 = 192000),
-                        scheduleEffect(new WordSearch(demoConfig, "Amazing work\n\nThank you Chris Newland\n\nalias @chriswhocodes"), t9, t11 = 265000),
-                        scheduleEffect(new SnowfieldSprite(demoConfig), t10 = t9 + 10000, t11)
+                        scheduleEffect(new RotateAddOnEffect(new FractalRings(demoConfig), 24300, 1, t3 = 32000, -1), t1, t2 = 40000),
+                        scheduleEffect(new Sierpinski(demoConfig), t3, t2),
+                        scheduleEffect(new Mandelbrot(demoConfig), t2, t4 = 64000),
+                        scheduleEffect(new TextWaveSprite(demoConfig, new String[] {"Realtime computation"}, demoConfig.getHeight() - 200, 0.8, 10), t2 + 2000, t5 = 64000),
+                        scheduleEffect(new Checkerboard(demoConfig), t4, t5 = 70000),
+                        scheduleEffect(new Chord(demoConfig, Color.ORANGE), 48300, t5),
+                        scheduleEffect(new Glowboard(demoConfig), t5, t6 = 96000),
+                        scheduleEffect(new SineLines(demoConfig), t5, t6),
+                        scheduleEffect(new Tiles(demoConfig), t6, t7 = 160200),
+                        scheduleEffect(new Spin(demoConfig), t7, t8 = 192000),
+                        scheduleEffect(new TextRing(demoConfig, new TextRing.RingData[] {
+                                new TextRing.RingData("Coded    in    Java    and    JavaFX", 300, 0.15, 1, 3, 2)}), t7 + 5000, t8),
+                        scheduleEffect(new WordSearch(demoConfig, "Amazing work\n\nThank you Chris Newland\n\nalias @chriswhocodes"), t8, t9 = 265000),
+                        scheduleEffect(new SnowfieldSprite(demoConfig), t8 + 10000, t9)
                         ));
                 root.getChildren().setAll(demoFX.getPane());
                 demoFX.runDemo();
