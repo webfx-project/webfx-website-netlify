826a3f4dab89ffc4e33f8fc6c837dc4e8c25539e
salmonb <maintainer@webfx.dev>
2022-09-03 22:04:25 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/c5d622061f4e3e139ff0e73348058acccab76fde

--- dev/webfx/demo/demofx/DemoFXApplication.java
+++ dev/webfx/demo/demofx/DemoFXApplication.java
@@ -45,8 +45,15 @@ public class DemoFXApplication extends Application {
         stage.setTitle("DemoFX");
         stage.setScene(scene);
         stage.show();
+        runIntroDemo();
+        GraphicsContext loadingContext = new Canvas().getGraphicsContext2D();
+        loadingContext.drawImage(quaver, 0, 0);
+        loadingContext.drawImage(quaver2, 0, 0);
+        loadingContext.drawImage(tiger, 0, 0);
+    }
+
+    private void runIntroDemo() {
         introDemo = newIntroDemo();
-        actualDemo = newActualDemo();
         root.getChildren().setAll(introDemo.getPane());
         introDemo.runDemo();
         root.setOnMousePressed(e -> { // Using setOnMousePressed() because sound doesn't start on iPad if using setOnMouseClicked()
@@ -60,15 +67,13 @@ public class DemoFXApplication extends Application {
             actualDemo.runDemo();
             started = true;
         });
-        GraphicsContext loadingContext = new Canvas().getGraphicsContext2D();
-        loadingContext.drawImage(quaver, 0, 0);
-        loadingContext.drawImage(quaver2, 0, 0);
-        loadingContext.drawImage(tiger, 0, 0);
+        actualDemo = newActualDemo();
     }
 
     private DemoConfig newDemoConfig(String audioResource) {
         DemoConfig demoConfig = new DemoConfig(scene.getWidth(), scene.getHeight());
         demoConfig.setAudioFilename(Resource.toUrl(audioResource, DemoFXApplication.class));
+        demoConfig.setAudioVolume(0.8); // To prevent saturation during rocky music
         return demoConfig;
     }
 
@@ -81,52 +86,72 @@ public class DemoFXApplication extends Application {
 
     private DemoFX newActualDemo() {
         return new DemoFX(newDemoConfig("DemoFX3.mp3"), (IEffectFactory) demoConfig -> dev.webfx.platform.util.collection.Collections.listOf(
+                // Starting sequence: Star field
                 scheduleEffect(new StarfieldSprite(demoConfig), 0, t1 = 15820),
 
+                // Fractal sequence:
+                // 1) Fractal rings
                 scheduleEffect(new RotateAddOnEffect(new FractalRings(demoConfig), 23920, -1, t2 = 32000, 1), t1, t3 = 39900),
-
+                // 3) Mandelbrot (declared before 2) so it is displayed behind Sierpinski)
                 scheduleEffect(new Mandelbrot(demoConfig), t3, t6 = 80000),
+                // 2) Sierpinski
                 scheduleEffect(new Sierpinski(demoConfig), t2, t4 = 47850),
+                // 4) Chord on top of Mandelbrot (still running)
                 scheduleEffect(new Chord(demoConfig, Color.ORANGE), t4, t5 = 64000),
+                // Text wave between 4) and 5)
                 scheduleEffect(new TextWaveSprite(demoConfig, new String[] {"Realtime Mandelbrot computation"}, demoConfig.getHeight() - 200, 0.8, 10), t4 + 2000, t5),
-                scheduleEffect(new Concentric(demoConfig, 20, createTintedQuaver(Color.web("#00ACEB")), createTintedQuaver(Color.web("#00A656")), createTintedQuaver(Color.web("#FCE400")), createTintedQuaver(Color.web("#F36126")), createTintedQuaver(Color.web("#CE0166")), createTintedQuaver(Color.web("#91248D"))).setPulseTimes(64000, 64257, 64500, 64758, 65268, 66000, 66497, 66753, 67258, 68011, 68261, 68754, 69258, 69751, 70251, 70754, 71246, 72005, 72259, 72756, 73254, 73754, 74018, 76001, 76254, 76756, 77256, 77751, 78503, 78754, 79255, 80000, 80256, 80755, 81256, 81650), t5, t6),
+                // 5) Concentric colored quavers on top of Mandelbrot (still running)
+                scheduleEffect(new Concentric(demoConfig, 20, createTintedQuaver(Color.web("#00ACEB")), createTintedQuaver(Color.web("#00A656")), createTintedQuaver(Color.web("#FCE400")), createTintedQuaver(Color.web("#F36126")), createTintedQuaver(Color.web("#CE0166")), createTintedQuaver(Color.web("#91248D")))
+                        // Pulse times matching the music:
+                        .setPulseTimes(64000, 64257, 64500, 64758, 65268, 66000, 66497, 66753, 67258, 68011, 68261, 68754, 69258, 69751, 70251, 70754, 71246, 72005, 72259, 72756, 73254, 73754, 74018, 76001, 76254, 76756, 77256, 77751, 78503, 78754, 79255, 80000, 80256, 80755, 81256, 81650), t5, t6),
 
+                // Audio spectrum sequence:
+                // 1) Equaliser with falling quavers on top of a glow board
                 scheduleEffect(new Glowboard(demoConfig, 32), t6, t7 = 96000),
                 scheduleEffect(new FadeOutAddOnEffect(new Falling(demoConfig, purpleQuaver = createTintedQuaver(Color.web("#BD4AEA")), quaver2), 1000), t6, 88000 + 1000),
                 scheduleEffect(new Equaliser(demoConfig), t6, 88000),
-
+                // 2) Sine lines with volume effect, on top of a sea of quavers, on top of the same glow board (still running)
                 scheduleEffect(new Sea(demoConfig, purpleQuaver), 88000, t7),
                 //scheduleEffect(new VolumeAddOnEffect(new SineLines(demoConfig), 4), 88000, t7),
                 scheduleEffect(new SineLines(demoConfig), 88000, t7),
 
+                // Rocky music 1 sequence:
+                // 2) Moire2 effect (declared before 1) to be behind the still running JavaFX tiles)
                 scheduleEffect(new Moire2(demoConfig), 112000, t8 = 128000),
+                // 1) JavaFX tiles
                 scheduleEffect(new Tiles(demoConfig), t7, t8),
-
+                // 3) Moire effect on top of a red checkerboard
                 scheduleEffect(new Checkerboard(demoConfig, Color.RED), t8, t9 = 136000),
                 scheduleEffect(new Moire(demoConfig), t8, t9),
-
+                // 4) Rings effect on top of an orange checkerboard
                 scheduleEffect(new Checkerboard(demoConfig, Color.ORANGE), t9, t10 = 144000),
                 scheduleEffect(new Rings(demoConfig), t9, t10),
-
+                // 5) Mask stack effect (with tiger image), on top of a blue checkerboard
                 scheduleEffect(new Checkerboard(demoConfig), t10, t11 = 152000),
                 scheduleEffect(new MaskStack(demoConfig, tiger), t10, t11 = 152000),
-
+                // 6) Twister effect, on top of a cyan blue checkerboard
                 scheduleEffect(new Checkerboard(demoConfig, Color.web("#00C2B6")), t10 = 152000, t11 = 159874),
                 scheduleEffect(new Twister(demoConfig, Color.web("#FCC738"), Color.web("#E6933F"), Color.web("#FC6038"), Color.web("#F2386E")), t10 = 152000, t11 = 159874),
 
-                scheduleEffect(new Moire2(demoConfig), t13 = 175000, t14 = 192000),
-                //scheduleEffect(new Burst(demoConfig, createTintedQuaver(Color.PURPLE), 4), 179750, t14),
+                // Rocky music 2 sequence:
+                // 1) Spin effect (Java logo) with a fading out effect at the end
+                scheduleEffect(new FadeOutAddOnEffect(new Spin(demoConfig), 2000), t12 = 159874, (t13 = 175000) + 2000),
+                // 2) Text ring (Entirely in Java and JavaFX), on top of mandalas (starting later), on top of a Moire2 effect
+                scheduleEffect(new Moire2(demoConfig), t13, t14 = 192000),
                 scheduleEffect(new Mandala(demoConfig, 32), 179750, t14),
                 scheduleEffect(new TextRing(demoConfig, new TextRing.RingData[] {
                         new TextRing.RingData("Entirely    in    Java    and    JavaFX", 250, 0.13, -1, 3, 2)}), t13, t14),
-                scheduleEffect(new FadeOutAddOnEffect(new Spin(demoConfig), 2000), t12 = 159874, t13 + 2000),
 
+                // Ending sequence:
+                // 1) Amazing work (word search effect)
                 scheduleEffect(new FadeOutAddOnEffect(new WordSearch(demoConfig, "Amazing work\n\nThank you Chris Newland\n\nalias @chriswhocodes"), 2500), t14, (t15 = t14 + 18000) + 2000),
-                scheduleEffect(new FadeOutAddOnEffect(new Credits(demoConfig, (t16 = tend - 4 * (75 + 100 + 75)) - t15 - 1500), 2500), t15, t16),
+                // 3) Credits (declared before 2) so it's behind the snow)
+                scheduleEffect(new FadeOutAddOnEffect(new Credits(demoConfig, Color.web("#B0B0B0"), (t16 = tend - 4 * (75 + 100 + 75)) - t15 - 1500), 2500), t15, t16),
+                // 2) Snow field
                 scheduleEffect(new FadeOutAddOnEffect(new SnowfieldSprite(demoConfig), 2500), t14 + 8000, t16),
-                //scheduleEffect(new FadeOutAddOnEffect(new Burst(demoConfig), 2500), t14 + 18000, t15),
+                // 4) Thank you for watching (flash text)
                 scheduleEffect(new TextFlash(demoConfig, "Thank you for watching", false, 75, 100, 75), t16, tend)
-        ));
+        )).setOnCompleted(this::runIntroDemo);
     }
 
     private IEffect scheduleEffect(IEffect effect, long start, long stop) {
