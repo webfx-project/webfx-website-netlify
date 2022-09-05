f9f45c4a08d4ec198e7931f70ee8a640524f9d3c
salmonb <maintainer@webfx.dev>
2022-09-05 04:23:41 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/bfbc594908500332d43dffe85d9664351fe92200

--- dev/webfx/demo/demofx/DemoFXApplication.java
+++ dev/webfx/demo/demofx/DemoFXApplication.java
@@ -147,7 +147,7 @@ public class DemoFXApplication extends Application {
 
                 // Ending sequence:
                 // 1) Amazing work (word search effect)
-                scheduleEffect(new FadeOutAddOnEffect(new WordSearch(demoConfig, "Amazing work\n\nThank you Chris Newland\n\nalias @chriswhocodes"), 2500), t14, (t15 = t14 + 18000) + 2000),
+                scheduleEffect(new FadeOutAddOnEffect(new WordSearch(demoConfig, "Amazing work\n\nThank you Chris Newland\n\n@chriswhocodes"), 2500), t14, (t15 = t14 + 18000) + 2000),
                 // 3) Credits (declared before 2) so it's behind the snow)
                 scheduleEffect(new FadeOutAddOnEffect(new Credits(demoConfig, Color.web("#D0D0D0"), (t16 = tend - 4 * (75 + 100 + 75)) - t15), 1000), t15, t16),
                 // 2) Snow field
