4e9409353343aba30bcc1f26051364a891c3f828
salmonb <maintainer@webfx.dev>
2022-09-23 15:13:52 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/bec9b7da22f9baac160c7a64421cd13d9ff52191

--- dev/webfx/demo/demofx/DemoFXApplication.java
+++ dev/webfx/demo/demofx/DemoFXApplication.java
@@ -89,7 +89,7 @@ public class DemoFXApplication extends Application {
     private DemoFX newActualDemo() {
         return new DemoFX(newDemoConfig("DemoFX3.mp3"), (IEffectFactory) demoConfig -> Collections.listOf(
                 // Starting sequence: Star field
-                new FadeOutAddOnEffect(scheduleEffect(new StarfieldSprite(demoConfig), 0, (t1 = 15820) + 5000), 5000 /* 5s fadeout effect when fractal rings starts */),
+                new FadeOutAddOnEffect(scheduleEffect(new StarfieldSprite(demoConfig), 0, (t1 = 15820) + 5000), 3000 /* 3s fadeout effect 2s after fractal rings starts */),
 
                 // Fractal sequence:
                 // 1) Fractal rings
