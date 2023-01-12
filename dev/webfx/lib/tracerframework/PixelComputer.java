f1b3ee2a37fba9832931b5047fe7dbbfc07a7ab6
salmonb <maintainer@webfx.dev>
2023-01-12 16:08:12 +0000

    Update from https://github.com/webfx-demos/webfx-demo-raytracer/commit/93fc83120cdbc04e30e31e16a079cb2189aee6e4

---
+++ dev/webfx/lib/tracerframework/PixelComputer.java
@@ -0,0 +1,44 @@
+package dev.webfx.lib.tracerframework;
+
+import javafx.scene.paint.Color;
+import dev.webfx.platform.json.ReadOnlyJsonObject;
+import dev.webfx.platform.webworker.spi.base.JavaCodedWebWorkerBase;
+
+/**
+ * @author Bruno Salmon
+ */
+public interface PixelComputer {
+
+    int getPlacesCount();
+
+    int getFramesCount(int placeIndex);
+
+    TracerThumbnail getPlaceThumbnail(int placeIndex);
+
+    void setUsingWebAssembly(boolean usingWebAssembly);
+
+    boolean isUsingWebAssembly();
+
+    long getLastFrameIterations(); // TODO Find a way to remove this Mandelbrot specific method
+
+    void initFrame(int canvasWidth, int canvasHeight, int placeIndex, int frameIndex);
+
+    void endFrame();
+
+    // Pixel color extractor from the line storage (once it has been computed)
+    Color getPixelResultColor(int x, int y, Object linePixelResultStorage);
+
+    // API when working in UI thread or standard threads (not workers)
+
+    Object createLinePixelResultStorage(); // Empty storage for the line
+
+    void computeAndStorePixelResult(int x, int y, Object linePixelResultStorage);
+
+    // API when working with workers
+
+    Class<? extends JavaCodedWebWorkerBase> getWorkerClass();
+
+    ReadOnlyJsonObject getLineWorkerParameters(int y, boolean firstWorkerCall);
+
+    Object getLinePixelResultStorage(Object workerResult);
+}
